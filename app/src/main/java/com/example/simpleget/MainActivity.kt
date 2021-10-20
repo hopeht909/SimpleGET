d3f228dc70b4891adb56d71cdf6e7933f3258f7a
hopeht909 <77729536+hopeht909@users.noreply.github.com>
2021-10-20 13:03:29 +0300

    with Recycle View

--- app/src/main/java/com/example/simpleget/MainActivity.kt
+++ app/src/main/java/com/example/simpleget/MainActivity.kt
@@ -6,26 +6,31 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var rvMain: RecyclerView
+    private lateinit var rvAdapter: RVAdapter
+    private lateinit var info: ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //response text for the text view in the scrollView
-        val responseText = findViewById<View>(R.id.textView) as TextView
-     
+
+        info =  arrayListOf()
+        rvMain = findViewById(R.id.rvMain)
+        rvAdapter = RVAdapter(info)
+
         //an object from APIInterface to call the method
         val apiInterface = APIClient().getClient()?.create(APIInterface::class.java)
         //progress dialog to show that something happened
         val progressDialog = ProgressDialog(this@MainActivity)
         progressDialog.setMessage("Please wait")
         progressDialog.show()
-
 
         if (apiInterface != null) {
             apiInterface.getUser()?.enqueue(object : Callback<List<People.PeopleDetailed>> {
@@ -34,12 +39,12 @@ class MainActivity : AppCompatActivity() {
                     response: Response<List<People.PeopleDetailed>>
                 ) {
                     progressDialog.dismiss()
-                    var stringToBePritined: String? = ""
                     for (User in response.body()!!) {
-                        stringToBePritined =
-                            stringToBePritined + User.name + "\n"
+                        info.add(User.name.toString())
                     }
-                    responseText.text = stringToBePritined
+                    rvMain.adapter = rvAdapter
+                    rvMain.layoutManager = LinearLayoutManager(this@MainActivity)
+                    rvMain.adapter?.notifyDataSetChanged()
                 }
 
                 override fun onFailure(call: Call<List<People.PeopleDetailed>>, t: Throwable) {
---
+++ app/src/main/java/com/example/simpleget/RVAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.simpleget
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.simpleget.databinding.ItemRowBinding
+
+class RVAdapter(private val entries: ArrayList<String>): RecyclerView.Adapter<RVAdapter.ItemViewHolder>() {
+    class ItemViewHolder(val binding: ItemRowBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(
+            ItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val entry = entries[position]
+
+        holder.binding.apply {
+            tvItem.text = entry
+
+        }
+    }
+
+    override fun getItemCount() = entries.size
+
+    fun update(){
+        notifyDataSetChanged()
+    }
+}
